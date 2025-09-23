f9c339ffebb943a0795392400ab4e6c94f7280b1
Your Name <you@example.com>
2025-09-23 19:34:16 -0300

    Subo mi rama con el frontend

---
+++ frontend/js/password-toggle.js
@@ -0,0 +1,12 @@
+// frontend/js/password-toggle.js
+function togglePassword(inputId, btn){
+    const input = document.getElementById(inputId);
+    if (!input) return;
+    if (input.type === 'password') {
+      input.type = 'text';
+      btn.textContent = '🙈';
+    } else {
+      input.type = 'password';
+      btn.textContent = '👁️';
+    }
+  }
